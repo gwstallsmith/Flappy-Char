23a167c9f7920e281737c9e6e035c9aebd260a14
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-18 19:16:29 -0400

    Canvas.

---
+++ main.js
@@ -0,0 +1,26 @@
+let canvas
+function setup() {
+    canvas = createCanvas(100, 100)
+    canvas.parent('p5')
+}
+
+function preload() {
+
+}
+
+function keyPressed() {
+
+}
+
+function draw() {
+    frameRate(30)
+
+
+    text('a', 10, 10)
+
+
+}
+
+function main() {
+
+}
