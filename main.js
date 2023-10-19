1d6b277d5349bc92cf9378d6ea1c70b30cbef136
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-19 11:31:24 -0400

    Added primitive flapping and gravity.

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
+let CANVAS_HEIGHT = window.innerHeight * 19/20
+let CANVAS_WIDTH = window.innerWidth * 19/20
+
+let FRAME_RATE = 60;
+
+
 let canvas
 function setup() {
-    canvas = createCanvas(100, 100)
+    canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     canvas.parent('p5')
+
+    textSize(32  )
+
+    canvas.mouseClicked(flap)
+    
 }
 
 function preload() {
-
 }
 
 function keyPressed() {
-
+    if(keyCode === 32) {
+        flap();
+    }
 }
+
+//let x = 0;
+let y = CANVAS_HEIGHT/2;
 
 function draw() {
-    frameRate(30)
+    frameRate(FRAME_RATE)
+    background('white')
+    stroke('black')
+
+    text('@', CANVAS_WIDTH/3, y)
+
+    //x = x >= window.innerWidth ? 0 : ++x
 
 
-    text('a', 10, 10);
-
-
+    gravity();
 }
 
-function main() {
+function flap() {
+    if(y > 0)   y -= 100
+}
 
+function gravity() {
+    if(y < CANVAS_HEIGHT) { y += 5 }
 }
