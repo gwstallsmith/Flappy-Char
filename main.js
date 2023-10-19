518a910aa6db07b24b3d79bf81bba51bbf0ddd18
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-19 12:35:22 -0400

    Preliminary bird object.

--- main.js
+++ main.js
@@ -3,15 +3,53 @@ let CANVAS_WIDTH = window.innerWidth * 19/20
 
 let FRAME_RATE = 60;
 
+//let upVector = createVector(0, 100);
+
+class Bird {
+    constructor() {
+        this.y_ = CANVAS_HEIGHT/2
+        this.x_ = CANVAS_WIDTH
+
+        this.char_ = '@'
+    }
+
+
+    drawBird() {
+        text(this.char_, this.x_, this.y_);
+    }
+
+    flap() {
+        if(this.y_ > 0) {
+            this.y_ -= 100
+        }
+    }
+
+    gravity(gravity = 5) {
+        if(this.y_ < CANVAS_HEIGHT) {
+            this.y_ -= gravity;
+        }
+    }
+
+}
+
+
+
+
 
 let canvas
+let bird
+
 function setup() {
     canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     canvas.parent('p5')
 
-    textSize(32  )
+    textSize(32)
 
-    canvas.mouseClicked(flap)
+    bird = new Bird();
+
+
+
+    canvas.mouseClicked(bird.flap())
     
 }
 
@@ -20,30 +58,21 @@ function preload() {
 
 function keyPressed() {
     if(keyCode === 32) {
-        flap();
+        bird.flap();
     }
 }
 
-//let x = 0;
-let y = CANVAS_HEIGHT/2;
 
 function draw() {
     frameRate(FRAME_RATE)
     background('white')
     stroke('black')
 
-    text('@', CANVAS_WIDTH/3, y)
-
-    //x = x >= window.innerWidth ? 0 : ++x
-
-
-    gravity();
+    bird.drawBird();
+    
+    bird.gravity();
 }
 
-function flap() {
-    if(y > 0)   y -= 100
-}
-
-function gravity() {
-    if(y < CANVAS_HEIGHT) { y += 5 }
+function main() {
+    
 }
