e0301156fa126dcb71289d3a6fee70a3642ff5c7
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-20 11:20:44 -0400

    Added pipe class.

--- main.js
+++ main.js
@@ -3,62 +3,77 @@ let CANVAS_WIDTH = window.innerWidth * 19/20 > 500 ? 500 : window.innerWidth * 1
 
 let FRAME_RATE = 60;
 
-//let upVector = createVector(0, 100);
-
-
 class GameManager {
     constructor() { 
         this.gameObjects = []; 
     }
 
-    static Priority = {
-        LOW: 1,
-        MEDIUM: 2,
-        HIGH: 3
-    }
 
     queue(gameObject)   { 
-        this.gameObjects.push(gameObject);
-        this.changed = true;
+        this.gameObjects.push(gameObject)
     }
 
 }
 
 class GameObject {
     constructor (x, y) {
-        this.position = new p5.Vector(x, y);
+        this.position = new p5.Vector(x, y)
         this.collider = false;
-        this.priority = GameManager.Priority.LOW;
     }
 }
 
-class Bird extends GameObject{
+class Pipe extends GameObject {
     constructor() {
         super()
-        this.position = createVector(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2); 
+        this.position = createVector(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2)
+
+        this.char_ = '|'
+    }
+
+    update() {
+        this.position.add(pipeVelocity)
+    
+        if (this.position.x < 0) {
+          this.position.x = CANVAS_WIDTH // Keep the bird within the canvas.
+        }
+      }
+    
+    display() {
+        stroke('black');
+        text(this.char_, this.position.x, this.position.y)
+    }
+
+
+
+}
+
+class Bird extends GameObject {
+    constructor() {
+        super()
+        this.position = createVector(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2)
 
         this.char_ = '@'
     }
 
 
     update() {
-        velocity.add(gravity); // Add gravity to the velocity.
-        this.position.add(velocity); // Update bird's position based on velocity.
+        birdVelocity.add(gravity) // Add gravity to the velocity.
+        this.position.add(birdVelocity) // Update bird's position based on velocity.
     
         if (this.position.y > CANVAS_HEIGHT) {
-          this.position.y = CANVAS_HEIGHT; // Keep the bird within the canvas.
-          velocity.mult(0); // Reset velocity when it hits the ground.
+          this.position.y = CANVAS_HEIGHT // Keep the bird within the canvas.
+          birdVelocity.mult(0) // Reset velocity when it hits the ground.
         }
       }
     
-      display() {
+    display() {
         stroke('black');
-        text(this.char_, this.position.x, this.position.y);
-      }
+        text(this.char_, this.position.x, this.position.y)
+    }
     
-      flap() {
-        velocity.y = -5; // Give the bird an upward velocity to simulate a flap.
-      }
+    flap() {
+        birdVelocity.y = -5 // Give the bird an upward velocity to simulate a flap.
+    }
 
 }
 
@@ -66,26 +81,29 @@ class Bird extends GameObject{
 
 let canvas
 let bird
-
+let GameMan
+let pipe
 function setup() {
     canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     canvas.parent('p5')
 
     textSize(32)
 
-    bird = new Bird();
-
-    
+    GameMan = new GameManager()
+    bird = new Bird()
+    pipe = new Pipe();
 }
 
 function preload() {
     gravity = createVector(0, 0.2); // Define gravity as a vector.
-    velocity = createVector(0, 0); // Initialize velocity vector.
+    birdVelocity = createVector(0, 0); // Initialize velocity vector.
+    pipeVelocity = createVector(-1  , 0);
+
 }
 
 function keyPressed() {
     if(keyCode === 32) {
-        bird.flap();
+        bird.flap()
     }
 }
 
@@ -101,9 +119,10 @@ function draw() {
     stroke('black')
 
     bird.display();
-    
-    bird.update();
+    pipe.display();
 
+    bird.update();
+    pipe.update();
 
 }
 
