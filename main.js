c282acdaccaa6407ec4a6f9bfb2cf886ccff7255
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-23 12:20:47 -0400

    Added to pipe constructor.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const CANVAS_HEIGHT = window.innerHeight * 19/20 > 1000 ? 1000 : window.innerHeight * 19/20 
+const CANVAS_HEIGHT = window.innerHeight * 3/4 > 1000 ? 1000 : window.innerHeight * 3/4  
 const CANVAS_WIDTH = window.innerWidth * 19/20 > 500 ? 500 : window.innerWidth * 19/20
 
 const FONT_SIZE = 28;
@@ -6,8 +6,7 @@ const FONT_SIZE = 28;
 const FRAME_RATE = 60;
 
 // PC == Pipe Chars
-const PC = [' ', '┃', '━', '┏', '┓', '└', '┘']
-
+const PC = [' ', '┃', '━', '┏', '┓', '┗', '┛']
 
 
 class GameManager {
@@ -16,39 +15,77 @@ class GameManager {
     }
 
 
-    queue(gameObject)   { 
-        this.gameObjects.push(gameObject)
-    }
 
 }
 
 class GameObject {
     constructor (x, y) {
-        this.position = new p5.Vector(x, y)
+        this.position_ = new p5.Vector(x, y)
         this.collider = false;
     }
-}
 
-class Pipe extends GameObject {
-    constructor() {
-        super()
-        this.position = createVector(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2)
-
-        this.char_ = PC[3] + PC[2] + PC[4] + '\n' + PC[1] + PC[0] + PC[0] + PC[1] + '\n' + PC[1] + PC[0] + PC[0]  + PC[1]
+    static PipeParts = {
+        TOP: PC[3] + PC[2] + PC[4] + '\n',
+        BOTTOM: PC[5] + PC[2] + PC[6] + '\n',
+        SIDE: PC[1] + PC[0] + PC[0] + PC[1] + '\n',
+        AIR: '\n'
     }
 
     update() {
-        this.position.add(pipeVelocity)
+        this.position_.add(pipeVelocity)
     
-        if (this.position.x < 0) {
-          this.position.x = CANVAS_WIDTH // Keep the bird within the canvas.
+        if (this.position_.x < 0) {
+          this.position_.x = CANVAS_WIDTH // Keep the bird within the canvas.
         }
       }
     
     display() {
         stroke('black');
-        text(this.char_, this.position.x, this.position.y)
+        text(this.char_, this.position_.x, this.position_.y)
     }
+
+
+}
+
+class Pipe extends GameObject {
+    constructor() {
+        super()
+        this.position_ = createVector(CANVAS_WIDTH, -FONT_SIZE)
+
+        this.opening_ = CANVAS_HEIGHT/2//Math.ceil(CANVAS_HEIGHT * Math.random())
+
+        for(let i = -FONT_SIZE; i < CANVAS_HEIGHT + FONT_SIZE; i += FONT_SIZE) {
+
+            if(Math.abs(this.opening_ - i) < FONT_SIZE) {
+                this.char += GameObject.PipeParts.BOTTOM
+                
+                console.log('BOT', GameObject.PipeParts.BOTTOM)
+                   
+            } else if(Math.abs(this.opening_ - i) < 3*FONT_SIZE) {
+                this.char_ += GameObject.PipeParts.AIR
+                console.log('AIR')
+
+
+            } else {
+                this.char_ += GameObject.PipeParts.SIDE
+                console.log('SIDE')
+
+            }
+
+
+        }
+
+    }
+
+    update() {
+        this.position_.add(pipeVelocity) // Update bird's position based on velocity.
+    
+        if (this.position_.x < -100) {
+          this.position_.x = CANVAS_WIDTH // Keep the bird within the canvas.
+          this.opening_ = Math.ceil(CANVAS_HEIGHT * Math.random())
+        }
+      }
+
 
 
 
@@ -57,7 +94,7 @@ class Pipe extends GameObject {
 class Bird extends GameObject {
     constructor() {
         super()
-        this.position = createVector(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2)
+        this.position_ = createVector(CANVAS_WIDTH / 6, CANVAS_HEIGHT / 2)
 
         this.char_ = '@'
     }
@@ -65,18 +102,14 @@ class Bird extends GameObject {
 
     update() {
         birdVelocity.add(gravity) // Add gravity to the velocity.
-        this.position.add(birdVelocity) // Update bird's position based on velocity.
+        this.position_.add(birdVelocity) // Update bird's position based on velocity.
     
-        if (this.position.y > CANVAS_HEIGHT) {
-          this.position.y = CANVAS_HEIGHT // Keep the bird within the canvas.
+        if (this.position_.y > CANVAS_HEIGHT) {
+          this.position_.y = CANVAS_HEIGHT // Keep the bird within the canvas.
           birdVelocity.mult(0) // Reset velocity when it hits the ground.
         }
       }
     
-    display() {
-        stroke('black');
-        text(this.char_, this.position.x, this.position.y)
-    }
     
     flap() {
         birdVelocity.y = -5 // Give the bird an upward velocity to simulate a flap.
